2f2d59a143781a2fa80df8c1a5cc2a1e0ed71ed8
satyama22 <ksslakshmi2005@gmail.com>
2025-03-21 18:27:28 +0530

    rout handling using next

--- src/app.js
+++ src/app.js
@@ -2,25 +2,24 @@ const express = require("express");
 //console.log("server creation");
 const app=express();
 
-app.get("/user",(req,res)=>{
-    console.log("user data accessed successfully");
-    res.send({firstname:"akshaysaini",lastname:"kumar"});
-})
-app.post("/user",(req,res)=>{
-    console.log(req.body);
-    res.send("data saved successfully to database");
-})
-app.delete("/user",(req,res)=>{
-    
-    res.end("data deleted successfully");
-})
-app.delete("/test",(req,res)=>{
-    console.log("deleted ");
-    res.end("data deleted successfully");
-})
-app.use("/use" ,(req,res) => {
-    res.send("hello everyone hii students");
-})
+app.get("/user",
+    (req,res,next)=>{
+       console.log("route handler1");
+        //res.send("response1");
+        next();
+    },[
+    (req,res,next)=>{
+    console.log("route handler2");
+      //res.send("response1")
+      next();
+    },
+    (req,res)=>{
+     console.log("route handler3");
+     res.send("response5");
+    }]
+);
+
+
 app.listen(3000 , () => {
     console.log("server is created successfully on port number 3000 ");
 });
