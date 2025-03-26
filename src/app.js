bbc10c4d000eb08ba5ce4b6ed4115e751dbce073
satyama22 <ksslakshmi2005@gmail.com>
2025-03-26 14:22:55 +0530

    delete apis working with db

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app=express();
 const connectDB = require("./config/database");
 const User= require("./models/user.js");
+const { model } = require("mongoose");
 app.use(express.json());
 app.post("/signup",async (req,res)=>{
 
@@ -16,9 +17,43 @@ app.post("/signup",async (req,res)=>{
    }
 
 })
+app.get("/user",async(req,res)=>{
+    const userName=req.body.firstName;
+    try{ 
+        const users=await User.findOne({firstName:userName});
+        if(users.length===0){
+            console.log("user not found");
+        }else{
+            res.send(users);
+        }
+    }catch(err){
+        res.status(404).send("something went wrong"+err.message);
+    }
+   
+})
+app.get("/feed",async (req,res)=>{
+    try{ 
+        const users=await User.find({});
+        if(users.length===0){
+            console.log("user not found");
+        }else{
+            res.send(users);
+        }
+    }catch(err){
+        res.status(404).send("something went wrong"+err.message);
+    }
+})
 
-
-
+app.delete("/delete",async(req,res)=>{
+        const userId=req.body.userId;
+        try{
+            //const user= await User.findByIdAndDelete(userId);
+            const user= await User.findByIdAndDelete({_id:userId});
+            res.send("user deleted successfully");
+        }catch(err){
+            res.status(404).send("somethng went wrong"+err.message);
+        }
+})
 connectDB().then(()=>{
     console.log("database connection established");
 
